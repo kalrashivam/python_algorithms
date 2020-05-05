9b5597f2c096ab1db23c81876c53eb18225ae956
kalrashivam <shivamkalra2017@gmail.com>
2020-05-05 23:36:44 +0530

    updated find the smallest missing number

--- find_the_smallest_missing_number.py
+++ find_the_smallest_missing_number.py
@@ -7,7 +7,7 @@ def binary_search_modified(arr, start, end):
 
     mid = int((start + end) / 2)
 
-    if array[mid] == mid:
+    if arr[mid] == mid:
         return binary_search_modified(arr, mid+1, end)
 
     return binary_search_modified(arr, start, mid)
@@ -18,6 +18,6 @@ arr = [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 n = len(arr)
 
 # Sends the array and the size as arguments
-binary_search_modified(arr, 0, n-1)
-print("Array after pushing all zeros to end of array:")
-print(arr)
+missing_element = binary_search_modified(arr, 0, n-1)
+print("And the missing element is: %d", missing_element)
+
