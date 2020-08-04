ab33cda669f2212e64fb2a146c9b00b5b0caacef
kalrashivam <shivamkalra2017@gmail.com>
2020-08-04 22:22:40 +0530

    Add return array permutations

--- return_array_permutations.py
+++ return_array_permutations.py
@@ -1,5 +1,20 @@
 # You are given an array of integers. Return all the permutations of this
 # array.
 def permute(nums):
+    l = []
+
+    if len(nums) == 0:
+        return []
+
+    if len(nums) == 1:
+        return [nums]
+
+    for i in range(len(nums)):
+       left = nums[i]
+       right = nums[:i] + nums[i+1:]
+
+       for p in permute(right):
+           l.append([left] + p)
+    return l
 
 print(permute([1, 2, 3]))
